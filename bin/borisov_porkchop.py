b5a0b269cd164b992fa0e47c2e6c36d27ccddec1
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-04-07 16:45:28 +0200

    feat: expand direct mission chapter

--- bin/borisov_porkchop.py
+++ bin/borisov_porkchop.py
@@ -13,11 +13,11 @@ from poliastro.util import time_range
 def main():
 
     N = 75
-    launch_span = time_range("2018-01-01", end="2028-01-01", num_values=N)
+    launch_span = time_range("2016-01-01", end="2028-01-01", num_values=N)
     arrival_span = time_range("2032-01-01", end="2035-01-01", num_values=N)
 
     borisov_ephem = Ephem.from_horizons("C/2019 Q4", launch_span)
-    borisov = Orbit.from_ephem(Sun, borisov_ephem, Time("2018-01-01"))
+    borisov = Orbit.from_ephem(Sun, borisov_ephem, Time("2016-01-01"))
 
     _, ax = plt.subplots(figsize=(16, 8))
     porkchop = PorkchopPlotter(
@@ -26,11 +26,12 @@ def main():
     porkchop.plot(
         c3_levels=np.linspace(0, 12e3, 13) * u.km ** 2 / u.s ** 2,
         tof_levels=np.linspace(0, 20, 21) * u.year,
+        dv_levels=np.linspace(0, 60, 11) * u.km / u.s,
         plot_c3_lines=False,
         plot_tof_lines=True,
         plot_dv_lines=False,
         plot_av_lines=False,
-        title="Earth - 2I/Borisov transfers for years 2018 - 2028\nLaunch energy $C_3$"
+        title="Earth - 2I/Borisov transfers for years 2016 - 2028\nLaunch energy $C_3$ and required time of flight"
     )
     plt.savefig("fig/static/borisov/direct-transfer-porkchop.png", bbox_inches="tight")
     plt.show()
--- bin/oumuamua_porkchop.py
+++ bin/oumuamua_porkchop.py
@@ -13,15 +13,15 @@ from poliastro.util import time_range
 def main():
 
     N = 75
-    launch_span = time_range("2018-01-01", end="2028-01-01", num_values=N)
+    launch_span = time_range("2016-01-01", end="2028-01-01", num_values=N)
     arrival_span = time_range("2032-01-01", end="2035-01-01", num_values=N)
 
-    borisov_ephem = Ephem.from_horizons("'Oumuamua", launch_span)
-    borisov = Orbit.from_ephem(Sun, borisov_ephem, Time("2018-01-01"))
+    oumuamua_ephem = Ephem.from_horizons("'Oumuamua", launch_span)
+    oumuamua = Orbit.from_ephem(Sun, oumuamua_ephem, Time("2016-01-01"))
 
     _, ax = plt.subplots(figsize=(16, 8))
     porkchop = PorkchopPlotter(
-        Earth, borisov, launch_span, arrival_span, ax=ax,
+        Earth, oumuamua, launch_span, arrival_span, ax=ax,
     )
     porkchop.plot(
         c3_levels=np.linspace(0, 12e3, 13) * u.km ** 2 / u.s ** 2,
@@ -30,7 +30,7 @@ def main():
         plot_tof_lines=True,
         plot_dv_lines=False,
         plot_av_lines=False,
-        title="Earth - 1I/'Oumuamua transfers for years 2018 - 2028\nLaunch energy $C_3$"
+        title="Earth - 2I/Oumuamua transfers for years 2016 - 2028\nLaunch energy $C_3$ and required time of flight"
     )
     plt.savefig("fig/static/oumuamua/direct-transfer-porkchop.png", bbox_inches="tight")
     plt.show()
