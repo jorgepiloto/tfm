c42c949a0325de6ee69c5a57223130f036bacb9b
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-04-15 21:04:11 +0200

    fix: use high fidelity ephemerides

--- bin/borisov_orbit.py
+++ bin/borisov_orbit.py
@@ -1,20 +1,20 @@
 from astropy import units as u
 from astropy.time import Time
-from poliastro.bodies import Sun
-from poliastro.ephem import Ephem
-from poliastro.twobody import Orbit
-from poliastro.plotting.misc import plot_solar_system
-from poliastro.util import time_range
+
 from matplotlib import pyplot as plt
 
+from poliastro.frames import Planes
+from poliastro.ephem import Ephem
+from poliastro.plotting.misc import plot_solar_system
 
-discovery=Time("2019-08-30", scale="tdb")
-epochs = time_range("2014-01-01", end="2022-01-01")
-borisov_ephem = Ephem.from_horizons("C/2019 Q4", epochs)
-borisov = Orbit.from_ephem(Sun, borisov_ephem, epoch=discovery)
+borisov = Ephem.from_csv("bin/ephem/borisov.csv", plane=Planes.EARTH_ECLIPTIC)
+discovery = Time("2019-08-30", scale="tdb")
 
-plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU)
-plotter.plot(borisov, label="2I/Borisov", color="black")
+plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU, plane=Planes.EARTH_ECLIPTIC)
+borisov_orbit_lines, _ = plotter.plot_ephem(borisov, epoch=discovery, color="black", label="2I/Borisov")
+borisov_orbit_lines.set_linestyle("--")
+
 plotter.backend.ax.set_xlim(-3, 3)
 plotter.backend.ax.set_ylim(-3, 3)
 plt.savefig("fig/static/borisov/orbit.png", bbox_inches="tight")
+plotter.show()
--- bin/oumuamua_orbit.py
+++ bin/oumuamua_orbit.py
@@ -1,20 +1,20 @@
 from astropy import units as u
 from astropy.time import Time
-from poliastro.bodies import Sun
+
+from matplotlib import pyplot as plt
+
+from poliastro.frames import Planes
 from poliastro.ephem import Ephem
-from poliastro.twobody import Orbit
 from poliastro.plotting.misc import plot_solar_system
-from poliastro.util import time_range
-import matplotlib.pyplot as plt
 
+oumuamua = Ephem.from_csv("bin/ephem/oumuamua.csv", plane=Planes.EARTH_ECLIPTIC)
+discovery = Time("2017-10-19", scale="tdb")
 
-discovery=Time("2017-10-19", scale="tdb")
-epochs = time_range("2014-01-01", end="2022-01-01")
-oumuamua_ephem = Ephem.from_horizons("'Oumuamua", epochs)
-oumuamua = Orbit.from_ephem(Sun, oumuamua_ephem, epoch=discovery)
+plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU, plane=Planes.EARTH_ECLIPTIC)
+borisov_orbit_lines, _ = plotter.plot_ephem(oumuamua, epoch=discovery, color="black", label="1I/'Oumuamua")
+borisov_orbit_lines.set_linestyle("--")
 
-plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU)
-plotter.plot(oumuamua, label="1I/'Oumuamua", color="black")
 plotter.backend.ax.set_xlim(-2, 2)
 plotter.backend.ax.set_ylim(-2, 2)
 plt.savefig("fig/static/oumuamua/orbit.png", bbox_inches="tight")
+plotter.show()
