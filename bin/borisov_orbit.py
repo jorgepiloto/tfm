4fec452a5527d7dc4de1293c8a1960a6371cd83b
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-04-21 12:48:35 +0200

    feat: add views of the orbits

--- bin/borisov_orbit.py
+++ bin/borisov_orbit.py
@@ -10,11 +10,20 @@ from poliastro.plotting.misc import plot_solar_system
 borisov = Ephem.from_csv("bin/ephem/borisov.csv", plane=Planes.EARTH_ECLIPTIC)
 discovery = Time("2019-08-30", scale="tdb")
 
-plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU, plane=Planes.EARTH_ECLIPTIC)
-borisov_orbit_lines, _ = plotter.plot_ephem(borisov, epoch=discovery, color="black", label="2I/Borisov")
-borisov_orbit_lines.set_linestyle("--")
+view_and_limits = {
+    "xy": [[-3, 3], [-3, 3]],
+    "xz": [[-0.2, 3], [-0.2, 1]],
+    "yz": [[-0.2, 3], [-0.2, 1]],
+}
 
-plotter.backend.ax.set_xlim(-3, 3)
-plotter.backend.ax.set_ylim(-3, 3)
-plt.savefig("fig/static/borisov/orbit.png", bbox_inches="tight")
-plotter.show()
+for view, (xlim, ylim) in view_and_limits.items():
+    plotter = plot_solar_system(epoch=discovery, outer=False,
+                                length_scale_units=u.AU,
+                                plane=Planes.EARTH_ECLIPTIC, view=view)
+    borisov_orbit_lines, _ = plotter.plot_ephem(borisov, epoch=discovery, color="black", label="2I/Borisov")
+    borisov_orbit_lines.set_linestyle("--")
+    plotter.backend.ax.set_xlim(xlim)
+    plotter.backend.ax.set_ylim(ylim)
+    if view != "xy":
+        plotter.backend.ax.legend().remove()
+    plt.savefig(f"fig/static/borisov/orbit_{view}.png", bbox_inches="tight")
--- bin/oumuamua_orbit.py
+++ bin/oumuamua_orbit.py
@@ -10,11 +10,21 @@ from poliastro.plotting.misc import plot_solar_system
 oumuamua = Ephem.from_csv("bin/ephem/oumuamua.csv", plane=Planes.EARTH_ECLIPTIC)
 discovery = Time("2017-10-19", scale="tdb")
 
-plotter = plot_solar_system(epoch=discovery, outer=False, length_scale_units=u.AU, plane=Planes.EARTH_ECLIPTIC)
-borisov_orbit_lines, _ = plotter.plot_ephem(oumuamua, epoch=discovery, color="black", label="1I/'Oumuamua")
-borisov_orbit_lines.set_linestyle("--")
+view_and_limits = {
+    "xy": [[-2, 2], [-2, 2]],
+    "xz": [[-1.5, 1.5], [-0.5, 1]],
+    "yz": [[-1.5, 1.5], [-0.5, 1]],
+}
 
-plotter.backend.ax.set_xlim(-2, 2)
-plotter.backend.ax.set_ylim(-2, 2)
-plt.savefig("fig/static/oumuamua/orbit.png", bbox_inches="tight")
-plotter.show()
+for view, (xlim, ylim) in view_and_limits.items():
+    plotter = plot_solar_system(epoch=discovery, outer=False,
+                                length_scale_units=u.AU,
+                                plane=Planes.EARTH_ECLIPTIC, view=view)
+    borisov_orbit_lines, _ = plotter.plot_ephem(oumuamua, epoch=discovery, color="black", label="1I/'Oumuamua")
+    borisov_orbit_lines.set_linestyle("--")
+
+    plotter.backend.ax.set_xlim(xlim)
+    plotter.backend.ax.set_ylim(ylim)
+    if view != "xy":
+        plotter.backend.ax.legend().remove()
+    plt.savefig(f"fig/static/oumuamua/orbit_{view}.png", bbox_inches="tight")
