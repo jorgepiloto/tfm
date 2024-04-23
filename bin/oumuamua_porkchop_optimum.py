f22a791d1fc1e61e78504ba74e422772dfa35cbc
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-04-23 16:42:54 +0200

    feat: consider escape velocity

--- bin/oumuamua_porkchop_optimum.py
+++ bin/oumuamua_porkchop_optimum.py
@@ -2,25 +2,34 @@ from astropy import units as u
 from matplotlib import pyplot as plt
 import numpy as np
 
-from poliastro.ephem import Ephem
+from poliastro.bodies import Sun
 from poliastro.frames import Planes
+from poliastro.ephem import Ephem
+from poliastro.twobody import Orbit
+from poliastro.frames import Planes
+from poliastro.twobody.sampling import EpochsArray
 from poliastro.plotting.porkchop import PorkchopPlotter
 from poliastro.util import time_range
+from poliastro.maneuver import Maneuver
+from poliastro.plotting.misc import plot_solar_system
 
 
 def solve_porkchop(prograde=True):
     # Declare the launch and arrival spans
     N = 200
     launch_span = time_range("2016-10-01", end="2017-10-01", num_values=N, scale="tdb")
-    arrival_span = time_range("2017-09-12", end="2018-04-01", num_values=N, scale="tdb")
+    arrival_span = time_range("2017-09-12", end="2018-01-15", num_values=N, scale="tdb")
 
     # Load the ephemerides for the Earth and 'Oumuamua
     earth = Ephem.from_csv("bin/ephem/earth.csv", plane=Planes.EARTH_ECLIPTIC)
     oumuamua = Ephem.from_csv("bin/ephem/oumuamua.csv", plane=Planes.EARTH_ECLIPTIC)
 
+    # Compute the escape velocity
+    escape_velocity = 11.2 * u.km / u.s
+
     # Compute the porkchop plot
     return PorkchopPlotter(
-        earth, oumuamua, launch_span, arrival_span, prograde=prograde
+        earth, oumuamua, launch_span, arrival_span, prograde=prograde, escape_velocity=escape_velocity
     )
 
 def main():
@@ -80,6 +89,50 @@ def main():
     )
     plt.savefig(f"fig/static/oumuamua/direct-detailed-porkchop-avl.png", bbox_inches="tight")
 
+    # Compute optimum transfer orbit
+    earth_ephem = Ephem.from_csv("bin/ephem/earth.csv", plane=Planes.EARTH_ECLIPTIC)
+    oumuamua_ephem = Ephem.from_csv("bin/ephem/oumuamua.csv", plane=Planes.EARTH_ECLIPTIC)
+
+    # Define the desired times
+    at_launch = porkchop.launch_date_at_c3_launch_min
+    at_arrival = porkchop.arrival_date_at_c3_launch_min
+    epochs = time_range(at_launch, end=at_arrival, num_values=1000, scale="tdb")
+
+    # Build associated orbits
+    earth = Orbit.from_ephem(Sun, earth_ephem, epoch=at_launch)
+    oumuamua = Orbit.from_ephem(Sun, oumuamua_ephem, epoch=at_arrival)
+
+    # Compute the transfer maneuver
+    lambert = Maneuver.lambert(earth, oumuamua)
+    print(f"Total cost: {lambert.get_total_cost()}")
+    print(f"Total time: {lambert.get_total_time().to(u.day)}")
+    for _, dv in lambert.impulses:
+        print(f"Impulses: {dv.to(u.km / u.s)}")
+        print(f"Cost: {(sum(impulse ** 2 for impulse in dv) ** 0.5).to(u.km/u.s)}")
+    transfer, _ = earth.apply_maneuver(lambert, intermediate=True)
+
+    # Model the transfer orbit as an ephem
+    transfer_ephem = transfer.to_ephem(strategy=EpochsArray(epochs))
+
+    view_and_limits = {
+        "xy": [[-2, 2], [-2, 2]],
+        "xz": [[-1.5, 1.5], [-0.5, 1]],
+        "yz": [[-1.5, 1.5], [-0.5, 1]],
+    }
+
+    for view, (xlim, ylim) in view_and_limits.items():
+        plotter = plot_solar_system(epoch=at_launch, outer=False,
+                                    length_scale_units=u.AU,
+                                    plane=Planes.EARTH_ECLIPTIC, view=view)
+        plotter.plot_ephem(transfer_ephem, label="Transfer orbit", color="red")
+        oumuamua_lines, _ = plotter.plot_coordinates(oumuamua_ephem.sample(epochs), position=oumuamua_ephem.rv(at_arrival)[0], label="1I/'Oumuamua at arrival", color="black")
+        oumuamua_lines.set_linestyle("--")
+
+        plotter.backend.ax.set_xlim(xlim)
+        plotter.backend.ax.set_ylim(ylim)
+        if view != "xy":
+            plotter.backend.ax.legend().remove()
+        plt.savefig(f"fig/static/oumuamua/direct-optimum-transfer-{view}.png", bbox_inches="tight")
 
 if __name__ == "__main__":
     main()
